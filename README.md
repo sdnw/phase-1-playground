a7200e7e613c76285b0b07eae07dceddbc880f21
flatlandnick <nicholas.lunn@flatironschool.com>
2022-06-07 07:48:15 -0500

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Phase 1 Playground
+
+Use this repository to practice writing code. The best way to learn how to code is to consistenly write code.
---
+++ index.js
@@ -0,0 +1,7 @@
+// all code written here is accessible in the browser console through the connection to our index.html file
+
+let cohort = "no name :(";
+
+let header = document.querySelector("h1");
+
+console.log(header);
